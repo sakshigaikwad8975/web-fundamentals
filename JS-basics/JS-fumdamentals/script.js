53e96f35026b87601ef8129b9621298ff80444ef
sakshigaikwad8975 <gaikwadsakshi8975@gmail.com>
2026-03-25 17:21:34 +0530

    Practice Questions Uploaded.

--- JS-basics/JS-fumdamentals/script.js
+++ JS-basics/JS-fumdamentals/script.js
@@ -6,7 +6,7 @@ if (num%2 ===0) {
     console.log(`${num} is an even number.`);
 }
 else{
-    console.log(`${num} is not an even number.`)
+    console.log(`${num} is odd number.`);
 }
 
 // Q2: Largest of Three Numbers
@@ -15,11 +15,53 @@ else{
 let a = 4;
 let b = 7;
 let c = 1;
-if (a>b && a>c) {
-    console.log(`${a} is largest number.`)
+if (a>=b && a>=c) {
+    console.log(`${a} is largest number.`);
 } 
-else if (b>a && b>c) {
-    console.log(`${b} is largest number.`)
+else if (b>=a && b>=c) {
+    console.log(`${b} is largest number.`);
 } else {
-    console.log(`${c} is the largest number.`)
+    console.log(`${c} is the largest number.`);
+}
+
+// Q3: Grade System
+// Write a program to assign grades based on marks:
+// - 90 and above → Grade A
+// - 75 to 89 → Grade B
+// - 50 to 74 → Grade C
+// - Below 50 → Fail
+
+let marks = 91;
+if(marks >= 90) {
+    console.log(" Grade A");
+}
+else if(marks >= 75) {
+    console.log("Grade B");
+}
+else if(marks >= 50) {
+    console.log("Grade C");
+}
+else{
+    console.log("Fail.");
+}
+
+// Q4: Username Validation
+// Write a program to check if a username is valid:
+// - It must start with a letter
+// - It must be at least 5 characters long
+// Print "Valid" or "Invalid".
+
+let username = "Sakshi";
+let firstChar = username[0];
+
+if (
+    (
+        (firstChar >= 'a' && firstChar <= 'z') ||
+        (firstChar >= 'A' && firstChar <= 'Z')
+    ) &&
+    username.length >= 5
+) {
+    console.log("Valid");
+} else {
+    console.log("Invalid");
 }
