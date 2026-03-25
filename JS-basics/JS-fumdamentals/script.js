f6820a99ca1fdaa6b6e435182b9610fdcd5e18a0
sakshigaikwad8975 <gaikwadsakshi8975@gmail.com>
2026-03-25 16:47:56 +0530

    JS Basics

---
+++ JS-basics/JS-fumdamentals/script.js
@@ -0,0 +1,10 @@
+// Q1: Even or Odd
+// Write a program to check whether a given number is even or odd.
+// Print "Even" if the number is divisible by 2, otherwise print "Odd".
+let num = 8;
+if (num%2 ===0) {
+    console.log(`${num} is an even number.`);
+}
+else{
+    console.log(`${num} is not an even number.`)
+}
